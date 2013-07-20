90dc505ae72a6972cad99975c7d680e5ee261cac
adasolcan <adasolcan@yahoo.com>
2013-07-20 23:16:44 +0300

    My first RoR app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorapp::Application.config.secret_token = '2da1d4b84ce32ee8ae9447ba5172a013e0811c35ccf480a48f2f4202641143f8c118522e407b486603366bb3da9f4abf3ab100facb309da8b263fd5296ca5483'
